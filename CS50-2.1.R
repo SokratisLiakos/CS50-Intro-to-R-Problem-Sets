6858bd80d692f8eaa3f0d2fde22b621a54a95e4e
Sokratis Liakos <140337183+SokratisLiakos@users.noreply.github.com>
2024-10-23 00:22:26 +0300

    Update CS50-2.1.R

--- CS50-2.1.R
+++ CS50-2.1.R
@@ -1,4 +1,5 @@
 #Read the given csv files
+
 books<-read.csv("books.csv")
 authors<-read.csv("authors.csv")
 
@@ -14,4 +15,18 @@ the_musician_book<-subset(books,year=="1613")
 
 the_traveler_book<-subset(books,(author=="Elena Petrova" | author=="Lysandra Silverleaf") & year=="1775")
 
-#
+#Find the Painter's book
+
+the_painter_book<-subset(books,(pages>200 & pages<300)&(year==1990 | year==1992))
+
+#Find the Scientist's book
+
+the_scientist_book<-subset(books,grepl("Quantum Mechanics",title))
+
+#Merging both csv files based on author
+
+books_authors<-merge(books,authors,by="author")
+
+#Find the Teacher's book
+
+the_teacher_book<-subset(books_authors,(year>=1700 & year<=1799 & hometown=="Zenthia"))
