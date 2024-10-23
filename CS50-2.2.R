ede6d2d0ced839adf10d7d1bbf19c1dd4e00497b
Sokratis Liakos <140337183+SokratisLiakos@users.noreply.github.com>
2024-10-23 12:46:17 +0300

    Update CS50-2.2.R

--- CS50-2.2.R
+++ CS50-2.2.R
@@ -3,7 +3,6 @@ flavor<-readline("Flavor:")
 caffeine<-readline("Caffeine:")
 
 #Conditions for the user's input
-
 if (flavor!="Bold" && flavor!="Light")
 {
   cat("Enter either 'Bold' or 'Light' for flavor.")
